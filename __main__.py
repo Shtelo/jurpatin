3ece442db53e34c285c5ceaee263c1988afb3d1b
Sch <junhg0211@gmail.com>
2022-11-11 18:11:00 +0900

    role 명령어 추가
    닉네임에 따라서 자동으로 명령어를 적용해주는 명령어

--- __main__.py
+++ __main__.py
@@ -1,11 +1,12 @@
 import re
+from asyncio import sleep
 from sys import argv
 
 from discord import Intents, Interaction, Member
 from discord.ext.commands import Bot, when_mentioned
 from sat_datetime import SatDatetime
 
-from util import get_secret
+from util import get_secret, get_const
 
 intents = Intents.default()
 intents.members = True
@@ -41,6 +42,33 @@ async def id(ctx: Interaction, member: Member, role: int = 5):
 
     await member.edit(nick=f'{candidate} {name}')
     await ctx.response.send_message(f'이름을 변경했습니다.\n> `{member.display_name}` > `{candidate} {name}`')
+
+
+ROLE_ID_TABLE = (
+    get_const('role.harnavin'), get_const('role.erasheniluin'),
+    get_const('role.quocerin'), get_const('role.lofanin'), get_const('role.hjulienin')
+)
+
+
+@bot.tree.command(
+    name='role',
+    description='닉네임에 따라 로판파샤스 역할을 부여합니다.'
+)
+async def role_(ctx: Interaction, member: Member):
+    if (role_number := member.display_name[0]) not in '12345':
+        await ctx.response.send_message(f'닉네임이 학번으로 시작하지 않거나 역할 지급 대상이 아닙니다.')
+        return
+
+    role_index = int(role_number)-1
+    for i in range(role_index):
+        await sleep(0)
+        role = ctx.guild.get_role(ROLE_ID_TABLE[i])
+        if role in member.roles:
+            await member.remove_roles(role)
+    role = ctx.guild.get_role(ROLE_ID_TABLE[role_index])
+    await member.add_roles(role)
+
+    await ctx.response.send_message(f'역할을 부여했습니다.')
 
 
 if __name__ == '__main__':
