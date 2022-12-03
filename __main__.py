6cd26398d648011eb756cb96781a9e5663f32ff7
Sch <junhg0211@gmail.com>
2022-12-03 13:09:58 +0900

    함수 이름 겹치는 것 수정

--- __main__.py
+++ __main__.py
@@ -33,7 +33,7 @@ async def on_member_join(member: Member):
     name = member.display_name if DECORATED_NICK_RE.match(member.display_name) is None else member.display_name[8:]
     nick = f'{candidate} {name}'
     await member.edit(nick=nick)
-    await give_role(member, nick[0], member.guild)
+    await assign_role(member, nick[0], member.guild)
 
 
 DECORATED_NICK_RE = re.compile(r'^\d{7} .+$')
@@ -98,7 +98,7 @@ ROLE_ID_TABLE = (
     get_const('role.hjulienin'))
 
 
-async def give_role(member: Member, role_number: str, guild: Guild):
+async def assign_role(member: Member, role_number: str, guild: Guild):
     role_index = int(role_number) - 1
     for i in range(role_index):
         await sleep(0)
