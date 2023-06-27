b39e8927dda47035ab33618d53782784b2878f47
Sch <junhg0211@gmail.com>
2023-06-27 11:40:04 +0900

    reset database connection every 1 hour

--- util/db.py
+++ util/db.py
@@ -1,18 +1,39 @@
+from datetime import datetime, timedelta
 from typing import Optional, Any
 
-from pymysql import connect
+from pymysql import connect, Connection
 
 from util import get_secret
 
-database = connect(
-    host=get_secret('database.host'),
-    user=get_secret('database.user'),
-    password=get_secret('database.password'),
-    database=get_secret('database.database'),
-)
+
+_get_connection_cache: Optional[Connection] = None
+_get_connection_last_used = None
+def get_connection():
+    global _get_connection_cache, _get_connection_last_used
+    now = datetime.now()
+
+    # if old connection
+    if _get_connection_last_used is not None \
+            and now - _get_connection_last_used > timedelta(hours=1):
+        _get_connection_cache.close()
+        _get_connection_cache = None
+
+    # if connection does not exist
+    if _get_connection_cache is None:
+        _get_connection_cache = connect(
+            host=get_secret('database.host'),
+            user=get_secret('database.user'),
+            password=get_secret('database.password'),
+            database=get_secret('database.database'),
+        )
+        print("NEW")
+        _get_connection_last_used = now
+
+    return _get_connection_cache
 
 
 def get_money(user_id: int) -> Optional[int]:
+    database = get_connection()
     with database.cursor() as cursor:
         cursor.execute('SELECT money FROM money WHERE id = %s', (user_id,))
         data = cursor.fetchone()
@@ -25,18 +46,21 @@ def get_money(user_id: int) -> Optional[int]:
 
 
 def create_account(user_id: int) -> None:
+    database = get_connection()
     with database.cursor() as cursor:
         cursor.execute('INSERT INTO money (id) VALUES (%s)', (user_id,))
         database.commit()
 
 
 def set_money(user_id: int, money: int) -> None:
+    database = get_connection()
     with database.cursor() as cursor:
         cursor.execute('UPDATE money SET money = %s WHERE id = %s', (money, user_id))
         database.commit()
 
 
 def add_money(user_id: int, money: int) -> None:
+    database = get_connection()
     with database.cursor() as cursor:
         cursor.execute('INSERT INTO money (id, money) VALUES (%s, %s) '
                        'ON DUPLICATE KEY UPDATE money = money + %s',
@@ -44,19 +68,22 @@ def add_money(user_id: int, money: int) -> None:
         database.commit()
 
 
-def get_money_ranking(limit: int = 10) -> list[tuple[int, int, int]]:
+def get_money_ranking(limit: int = 10) -> tuple[tuple[Any, ...], ...]:
+    database = get_connection()
     with database.cursor() as cursor:
         cursor.execute('SELECT id, money, rank() OVER (ORDER BY money DESC) FROM money LIMIT %s', (limit,))
         return cursor.fetchall()
 
 
 def set_value(key: str, value: str) -> None:
+    database = get_connection()
     with database.cursor() as cursor:
         cursor.execute('INSERT INTO `values` (`key`, value) VALUES (%s, %s)', (key, value))
         database.commit()
 
 
 def get_value(key: str) -> Optional[str]:
+    database = get_connection()
     with database.cursor() as cursor:
         cursor.execute('SELECT value FROM `values` WHERE `key` = %s', (key,))
         data = cursor.fetchone()
@@ -68,12 +95,14 @@ def get_value(key: str) -> Optional[str]:
 
 
 def remove_value(key: str) -> None:
+    database = get_connection()
     with database.cursor() as cursor:
         cursor.execute('DELETE FROM `values` WHERE `key` = %s', (key,))
         database.commit()
 
 
 def get_inventory(user_id: int) -> dict[str, int]:
+    database = get_connection()
     with database.cursor() as cursor:
         cursor.execute('SELECT name, amount FROM inventory WHERE id = %s', (user_id,))
         # noinspection PyTypeChecker
@@ -81,6 +110,7 @@ def get_inventory(user_id: int) -> dict[str, int]:
 
 
 def set_inventory(user_id: int, name: str, amount: int) -> None:
+    database = get_connection()
     with database.cursor() as cursor:
         if amount:
             cursor.execute('INSERT INTO inventory (id, name, amount) VALUES (%s, %s, %s) '
@@ -91,12 +121,8 @@ def set_inventory(user_id: int, name: str, amount: int) -> None:
 
 
 def add_inventory(user_id: int, name: str, amount: int) -> None:
+    database = get_connection()
     with database.cursor() as cursor:
         cursor.execute('INSERT INTO inventory (id, name, amount) VALUES (%s, %s, %s) '
                        'ON DUPLICATE KEY UPDATE amount = amount + %s', (user_id, name, amount, amount))
         database.commit()
-
-
-if __name__ == '__main__':
-    from pprint import pprint
-    pprint(get_money_ranking())
