2433425ba46bb421269e65a46206bd7c9a413bd9
Sch <junhg0211@gmail.com>
2024-03-20 08:28:06 +0900

    Update util/db.py

--- util/db.py
+++ util/db.py
@@ -235,7 +235,7 @@ def add_ppl_history(date_: date, value: int):
 def add_issue_history(datetime_: datetime, value: int):
     database = get_connection()
     with database.cursor() as cursor:
-        cursor.execute('INSERT INTO issue_history VALUES (%s, %s)', value, datetime_)
+        cursor.execute('INSERT INTO issue_history VALUES (%s, %s)', (value, datetime_))
         database.commit()
 
 
